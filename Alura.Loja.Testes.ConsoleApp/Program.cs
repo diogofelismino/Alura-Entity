3de9383b93ae93753ce3edbd7f3095e784b66556
diogofelismino <94204823+diogofelismino@users.noreply.github.com>
2023-03-03 13:12:08 -0400

    Inicio do projeto

---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         using (var contexto = new LojaContex())
+            {
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+
+                //var p1 = produtos.First();
+                //p1.Nome = "Percy Jackson";
+
+                //contexto.SaveChanges();
+
+                Console.WriteLine("============================");
+
+                produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+
+
+
+            }  
+        }
+
+  
+    }
+}
